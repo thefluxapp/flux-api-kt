8ea0adfbe298381de51f48d38b10d14d9d3d862c
Andy Braga <bragovo@gmail.com>
2023-02-19 18:34:22 +0300

    It's Spring Time (#1)
    * Add Spring
    
    * Add ktfmt
    
    * Add Cache
    
    * Add Java
    
    * Format and rename
    
    * Try tests
    
    * Split jobs
    
    * Add H2 for tests

---
+++ src/test/kotlin/flux/app/api/ApplicationTests.kt
@@ -0,0 +1,10 @@
+package flux.app.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test fun contextLoads() {}
+}
